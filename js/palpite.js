0dc45a38aca9dc8a15fcf20ebdcc7837df314ee0
rankme-wilson <wilson.junior@rankme.com.br>
2022-09-30 15:49:58 -0300

    ex jogo de adivinhação

---
+++ js/palpite.js
@@ -0,0 +1,75 @@
+var numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+
+var palpites = document.querySelector('.palpites');
+var ultimoResultado = document.querySelector('.ultimoResultado');
+var baixoOuAlto = document.querySelector('.baixoOuAlto');
+
+var envioPalpite = document.querySelector('.envioPalpite');
+var campoPalpite = document.querySelector('.campoPalpite');
+
+var contagemPalpites = 1;
+var botaoReinicio;
+campoPalpite.focus();
+
+function conferirPalpite() {
+  var palpiteUsuario = Number(campoPalpite.value);
+  if (contagemPalpites === 1) {
+    palpites.textContent = 'Palpites anteriores: ';
+  }
+  palpites.textContent += palpiteUsuario + '. ';
+
+  if (palpiteUsuario === numeroAleatorio) {
+    ultimoResultado.textContent = 'Parabéns! Você acertou!';
+    ultimoResultado.classList.add("acerto");
+    baixoOuAlto.textContent = '';
+    configFimDeJogo();
+  } else if (contagemPalpites === 10) {
+    ultimoResultado.textContent = '!!! FIM DE JOGO!!!';
+    baixoOuAlto.textContent = '';
+    configFimDeJogo();
+  } else {
+    ultimoResultado.textContent = 'Que pena você errou!';
+    ultimoResultado.classList.add("erro");
+    if(palpiteUsuario < numeroAleatorio) {
+      baixoOuAlto.textContent = 'Seu palpite está muito baixo!'
+    } else if(palpiteUsuario > numeroAleatorio) {
+      baixoOuAlto.textContent = 'Seu palpite está muito alto!'
+    }
+  }
+
+  contagemPalpites++;
+  campoPalpite.value = '';
+  campoPalpite.focus();
+}
+
+envioPalpite.addEventListener('click', conferirPalpite);
+
+function configFimDeJogo() {
+  campoPalpite.disable = true;
+  envioPalpite.disable = true;
+  botaoReinicio = document.createElement('button');
+  botaoReinicio.textContent = 'Iniciar novo jogo';
+  document.body.appendChild(botaoReinicio);
+  botaoReinicio.addEventListener('click', reiniciarJogo);
+}
+
+function reiniciarJogo() {
+  contagemPalpites = 1;
+
+  var reinicarParas = document.querySelectorAll('.resultadoParas p');
+  for (var i = 0; i < reiniciarParas.length; i++) {
+    reinicarParas[i].textContent = '';
+  }
+
+  botaoReinicio.parentNode.removeChild(botaoReinicio);
+
+  campoPalpite.disabled = false;
+  envioPalpite.disabled = false;
+  campoPalpite.value = '';
+  campoPalpite.focus();
+
+  ultimoResultado.style.backgroundColor = 'white';
+
+  numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+}
+
